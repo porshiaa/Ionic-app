a3f47efa38cf48db3548809319117040c6e625f6
Porshia Athow <2421280a@student.gla.ac.uk>
2019-11-21 23:59:38 +0000

    add cards to gallery

--- src/app/gallery/gallery.page.ts
+++ src/app/gallery/gallery.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-gallery',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryPage implements OnInit {
 
-  constructor() { }
+  users: any[]=[];
 
-  ngOnInit() {
-  }
+  constructor(public http: HttpClient) {
+    
+    this.http.get("https://randomuser.me/api/?results=20").subscribe(data=>{
+      this.users = data["results"]
+      console.log(this.users)
+      
+      })
+
+
 
 }
+
+ngOnInit() {
+}
+
+   }
+
+
+
+
+
+
+
+
