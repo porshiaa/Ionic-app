57fbb557791f1811d902fac469ea04a69f8a7853
Porshia Athow <2421280a@student.gla.ac.uk>
2019-11-21 23:46:06 +0000

    delete homepage add features to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
-  {
-    path: 'login',
-    loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
-  },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadChildren:'./login/login.module#LoginPageModule'},
+
   {
     path: 'gallery',
-    loadChildren: () => import('./gallery/gallery.module').then( m => m.GalleryPageModule)
-  },
+    loadChildren:'./gallery/gallery.module#GalleryPageModule'},
 ];
 
 @NgModule({
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(public nav: NavController) { }
+
+  goToGallery(){
+    this.nav.navigateForward('/gallery')
+    console.log('galleryfunc')
+  }
 
   ngOnInit() {
   }
